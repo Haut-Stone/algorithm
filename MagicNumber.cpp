da642e4a7035a2a0fad15a5d982375e0693db062
Haut-Stone <haut.1604.stone@gmail.com>
2016-11-12 23:24:17 +0800

    添加了注释。。
    添加了注释。。

--- MagicNumber.cpp
+++ MagicNumber.cpp
@@ -1,4 +1,10 @@
-#include <algorithm>
+
+//
+// main.cpp
+// Insertion Sort
+// Created by ShiJiahuan on 11/11/2016.
+// Copyright ©️ 2016 ShiJiahuan. All rights reserved.
+//#include <algorithm>
 #include <cstdio>
 #include <iostream>
 using namespace std;
@@ -11,11 +17,12 @@ int main() {
   int L, R;
   int a[10010] = {0};
   for (int i = 1; i <= 10000; i++) {
-    a[i] = a[i - 1] + judge(i);
+    a[i] = a[i - 1] + judge(i); //将符合条件的个数存入数组中。
   }
   for (int i = 0; i < T; i++) {
     scanf("%d %d", &L, &R);
-    printf("%d\n", a[R] - a[L - 1]);
+    printf("%d\n",
+           a[R] - a[L - 1]); //这样循环时避免了区间部分重合时的重复计算。
   }
   return 0;
 }
@@ -27,12 +34,12 @@ int judge(int solo) {
   int temp;
   while (soloCopy) {
     temp = soloCopy % 10;
-    if (temp == 4 || temp == 6)
+    if (temp == 4 || temp == 6) //判断是否符合条件3
       flag = 1;
     sum += temp;
     soloCopy /= 10;
   }
-  if (sum % 3 != 0 || flag == 0) {
+  if (sum % 3 != 0 || flag == 0) { //判断是否符合条件2 & 3
     return 0;
   }
   while (solo % 2 == 0)
@@ -41,7 +48,7 @@ int judge(int solo) {
     solo /= 3;
   while (solo % 7 == 0)
     solo /= 7;
-  if (solo != 1)
+  if (solo != 1) //判断是否符合条件1
     return 0;
   return 1;
 }
