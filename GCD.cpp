5d53063b981591defa0e4d3923f33a85002da2cb
Haut-Stone <haut.1604.stone@gmail.com>
2016-12-23 21:48:55 +0800

    辗转相除法求最大公约数。
    三个数互相之间交换，画出来比较好理解

---
+++ GCD.cpp
@@ -0,0 +1,47 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2016 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2016-12-23 21:37:44
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2016-12-23 21:41:32
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+int gcd(int a, int b);
+
+int main(void)
+{
+	int T;
+	int ans;
+    int a;
+    int b;
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d %d", &a, &b);
+		ans = gcd(a, b);
+		printf("%d\n", ans);
+	}
+    return 0;
+}
+
+int gcd(int a, int b)
+{
+        int c = a % b;
+        while(c)
+        {
+                a = b;
+                b = c;
+                c = a % b;
+        }
+        return b;
+}
