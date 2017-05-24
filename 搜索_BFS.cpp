994c965bfea0e1469454aa1452a4fc062e7d8a4e
Haut-Stone <haut.1604.stone@gmail.com>
2017-05-24 14:04:46 +0800

    update

--- 搜索_BFS.cpp
+++ 搜索_BFS.cpp
@@ -7,16 +7,69 @@
 * @Author: Haut-Stone
 * @Date:   2017-04-20 19:08:52
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-05-02 21:24:38
+* @Last Modified time: 2017-05-23 19:36:10
 */
 
-int bfs()
-{
-	
-}
+//这个示例是二维地图bfs的大致步骤
+//
 
-int main(void)
-{
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <set>
+#define INPUT_TEST freopen("in.txt", "r", stdin)
+using namespace std;
 
-    return 0;
+const int N = 1010;
+
+struct Node
+{
+	int x;
+	int y;
+	int steps;
+};
+
+int num;
+int row, col;
+int direction[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+int bfs(int beginX, int beginY)
+{
+	int book[N][N];
+	memset(book, 0 ,sizeof(book));
+	queue<Node> Q;
+
+	Node solo;
+	solo.x = beginX;
+	solo.y = beginY;
+	solo.steps = 0;
+
+	Q.push(solo);
+	book[solo.x][solo.y] = 1;
+
+	while(!Q.empty()){
+		solo = Q.front();
+		Q.pop();
+
+		for(int i=0;i<4;i++){
+			int dx = solo.x + direction[i][0];
+			int dy = solo.y + direction[i][1];
+			//如果该点合法	，加入队列
+			if(dx >= 0 && dy >= 0 && dx <= row && dy <= col){
+				Node temp;
+				temp.x = dx;
+				temp.y = dy;
+				temp.steps = solo.steps + 1;
+				book[temp.x][temp.y] = 1;
+				Q.push(temp);
+			}
+		}
+	}
+	return 0;
 }
--- 最小生成树_Prim.cpp
+++ 最小生成树_Prim.cpp
@@ -7,7 +7,7 @@
 * @Author: Haut-Stone
 * @Date:   2017-03-31 19:04:34
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-05-02 21:24:35
+* @Last Modified time: 2017-05-24 13:20:12
 */
 #include <algorithm>
 #include <iostream>
@@ -16,7 +16,7 @@ using namespace std;
 const int N = 100;
 const int INF = (1<<20);
 
-int Imap[N][N];
+int Imap[N][N];//第一个点的编号从零开始！！
 int minCost[N];
 bool vis[N];
 int vertexNumber;
@@ -34,25 +34,25 @@ int prim()
 	while(true){
 
 		int minDis = INF;
-        int minPos = 0;
+		int minPos = 0;
 
-        for(int j=0;j<vertexNumber;j++){
-            if(minCost[j] < minDis && !vis[j]){//搜索到当前点的最短距离
-                minDis = minCost[j];
-                minPos = j;
-            }
-        }
+		for(int j=0;j<vertexNumber;j++){
+			if(minCost[j] < minDis && !vis[j]){//搜索到当前点的最短距离
+				minDis = minCost[j];
+				minPos = j;
+			}
+		}
 
-        if(minDis == INF) break;//如果所有的点都被找过
+		if(minDis == INF) break;//如果所有的点都被找过
 
-        vis[minPos] = 1;
-        ans += minDis;
-        for(int j=0;j<vertexNumber;j++){
-        	//将已搜索过的点视为一个整体,更新整体的值。
-            if(!vis[j] && minCost[j] > Imap[minPos][j]){
-                minCost[j] = Imap[minPos][j];
-            }
-        }
+		vis[minPos] = 1;
+		ans += minDis;
+		for(int j=0;j<vertexNumber;j++){
+			//将已搜索过的点视为一个整体,更新整体的值。
+			if(!vis[j] && minCost[j] > Imap[minPos][j]){
+				minCost[j] = Imap[minPos][j];
+			}
+		}
 	}
 	return ans;
 }
